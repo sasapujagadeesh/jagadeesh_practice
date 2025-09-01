f628141a9f989952cf92c86ab5c6cab63b8373c0
sasapujagadeesh <sasapujagadeeswararao@gmail.com>
2025-09-01 17:26:01 +0530

    zero one triangle

---
+++ c_practice/patterns/zeroonetriangle.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+	for(int i = 1;i<5;i++)
+	{
+		for(int j = 1;j<(6-i);j++)
+		{
+			printf("  ");
+		}
+		for(int j = 0;j<(2*i-1);j++)
+		{
+			printf("%d ",(j+1)%2);
+		}
+		printf("\n");
+	}
+}
